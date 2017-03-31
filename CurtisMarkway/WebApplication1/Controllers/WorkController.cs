87d68f03efa31e00740597b6e8d2d9de9c761759
Cmarkway <cjmdr2@mail.missouri.edu>
2017-03-31 14:35:02 -0500

    Navigation bar
    Got the controllers for handling actions with the navigation bar

---
+++ CurtisMarkway/WebApplication1/Controllers/WorkController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class WorkController : Controller
+    {
+        public ActionResult Experience()
+        {
+            return View();
+        }
+    }
+}
