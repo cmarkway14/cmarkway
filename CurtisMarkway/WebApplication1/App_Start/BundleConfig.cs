79b8bd1ccc9950492ee59bba8c5f00437e4d29f4
Cmarkway <cjmdr2@mail.missouri.edu>
2017-03-31 15:08:11 -0500

    Trying to figure out why changes aren't showing on gihtub

--- CurtisMarkway/WebApplication1/App_Start/BundleConfig.cs
+++ CurtisMarkway/WebApplication1/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace WebApplication1
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/LaunchPage.css"));
+                      "~/Content/LaunchPage.css",
+                      "~/Content/ContactPage.css"));
         }
     }
 }
